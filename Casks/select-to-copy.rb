e1294aed422675d53ee83fdb135cb2ca1177d7aa
liem.sanjaya <liem.sanjaya@gojek.com>
2026-03-22 23:06:29 +0700

    Update select-to-copy cask to v1.0.3

--- Casks/select-to-copy.rb
+++ Casks/select-to-copy.rb
@@ -1,6 +1,6 @@
 cask "select-to-copy" do
-  version "1.0.1"
-  sha256 "828bc6c95ee730fe3715da0363bf77b793850219c8f383bb5086f87a74368f9c"
+  version "1.0.3"
+  sha256 "3ddfc85dece4eaefe0220b4d823cdd865838cdabed14a531576b2a5398245e64"
 
   url "https://github.com/hindraxxx/select-to-copy/releases/download/v#{version}/SelectToCopy-macos-arm64.dmg"
   name "SelectToCopy"
